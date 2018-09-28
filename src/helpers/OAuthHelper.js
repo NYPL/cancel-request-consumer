b3830a729d16a1ff8fc09965ddac574aaa3e52d9
Daniel Appel <dan.s.appel@gmail.com>
2018-09-28 17:05:45 -0400

    Add basic auth methods to main oauth helper

--- src/helpers/OAuthHelper.js
+++ src/helpers/OAuthHelper.js
@@ -37,6 +37,7 @@ const fetchAccessToken = function (oauthUrl, clientId, clientSecret, scope, gran
   return axios.post(oauthUrl, qs.stringify(oAuthConfig))
   .then(result => {
     if (result.data && result.data.access_token) {
+      console.log(40, oauthUrl, result.data.access_token)
       return Promise.resolve(result.data.access_token);
     }
 
@@ -107,21 +108,93 @@ const fetchAccessToken = function (oauthUrl, clientId, clientSecret, scope, gran
   });
 }
 
-const handleAuthentication = async function (cachedToken, getNewTokenFn) {
+const getBasicAuthHeaders = function (clientId, clientSecret) {
+  return {"auth" : {"username" : clientId, "password" : clientSecret } }
+}
+
+const fetchSierraToken = function (authUrl, clientId, clientSecret, scope, grantType) {
+  if (!authUrl || typeof authUrl !== 'string' || authUrl.trim() === '') {
+    return Promise.reject(
+      new CancelRequestConsumerError('fetchSierraToken: the authUrl function parameter is not defined or invalid; must be of type string and not empty')
+    );
+  }
+
+  //const oAuthConfig = getOauthConfig(clientId, clientSecret, scope, grantType);
+  const headers = getBasicAuthHeaders(clientId, clientSecret);
+  return axios.post(authUrl + "token", {}, headers)
+  .then(result => {
+    if (result.data && result.data.access_token) {
+      return Promise.resolve(result.data.access_token);
+    }
+
+    return Promise.reject(
+      new CancelRequestConsumerError(
+        'fetchSierraToken: the AuthResponse object contained an undefined access_token property',
+        { type: 'invalid-access-token-response' }
+      )
+    );
+  })
+  .catch(error => {
+    let errorMessage = 'fetchSierraToken: An error occurred from the auth service';
+
+    if (error.response) {
+      const statusCode = error.response.status;
+      const statusText = error.response.statusText;
+      if (statusCode) {
+        errorMessage += `; the service responded with status code: (${statusCode})`;
+      }
+      if (statusText) {
+        errorMessage += ` and status text: (${statusText})`;
+      }
+
+      logger.error(errorMessage, { debugInfo: error.response });
+      return Promise.reject(
+        new CancelRequestConsumerError(
+          errorMessage,
+          {
+            type: 'auth-service-error',
+            statusCode: error.response.status || null
+          }
+        )
+      );
+    }
+
+    if (error.request) {
+      errorMessage += '; the request was made, no response received from Auth Service';
+
+      logger.error(errorMessage, { debugInfo: error.request });
+      return Promise.reject(
+        new CancelRequestConsumerError(
+          errorMessage,
+          {
+            type: 'auth-service-error',
+            debugInfo: error.request
+          }
+        )
+      );
+    }
+  });
+}
+
+const handleAuthentication = async function (cachedToken, getNewTokenFn, name) {
   if (cachedToken && typeof cachedToken === 'string' && cachedToken !== '') {
+    console.log('cached', name)
     // Re-use cached access token
     return {
       tokenType: 'cached-token',
-      token: cachedToken
+      token: cachedToken,
+      tokenName: name
     };
   }
 
   try {
     // Obtain a new access token
     const accessToken = await getNewTokenFn;
+    console.log(accessToken, name);
     return {
       tokenType: 'new-token',
-      token: accessToken
+      token: accessToken,
+      tokenName: name
     };
   } catch (e) {
     throw e;
@@ -131,5 +204,7 @@ const handleAuthentication = async function (cachedToken, getNewTokenFn) {
 export {
   getOauthConfig,
   fetchAccessToken,
-  handleAuthentication
+  handleAuthentication,
+  fetchSierraToken,
+  getBasicAuthHeaders
 }
