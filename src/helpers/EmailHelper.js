8578041bec2177efd2d2215d149d1736b23f711c
Daniel Appel <dan.s.appel@gmail.com>
2019-03-01 12:13:30 -0500

    Initial email test

---
+++ src/helpers/EmailHelper.js
@@ -0,0 +1,38 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+const params = {
+  Destination: {
+    ToAddresses: [
+      'danielappel@nypl.org'
+    ]
+  },
+  Message: {
+    Body: {
+      Text: {
+        Charset: "UTF-8",
+        Data: "Success!"
+      }
+    },
+    Subject: {
+      Charset: "UTF-9",
+      Data: "Success!"
+    },
+  },
+  Source: 'danielappel@nypl.org'
+}
+
+const sendEmail = () => {
+  const sendPromise = new AWS.SES({apiVersion: 2010-12-01}).sendEmail(params).promise();
+  sendPromise.then(
+    function(data){
+      console.log(data.MessageId);
+    }
+  ).catch(
+    function(err){
+      console.error(err, err.stack);
+    }
+  );
+}
+
+export default sendEmail;
